7e210a375346971503eeea7583510523894d632e
afthuuu <afthabnaydin@gmail.com>
2024-04-22 17:40:41 +0530

    changes on page

---
+++ src/components/page/StockAudit.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StockAudit = () => {
+  return (
+    <div>
+        <div className="flex h-screen absolute top-1/2 left-1/2 transform -translate-x-1/3 -translate-y-1/3 ">   
+            <div className=" h-48 w-48  bg-black shadow-md rounded-full"></div>
+        </div> 
+      
+    </div>
+  )
+}
+
+export default StockAudit
+export{};
